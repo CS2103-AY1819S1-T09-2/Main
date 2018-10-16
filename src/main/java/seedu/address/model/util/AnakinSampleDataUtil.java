0855d5a37f97989a44995472831a731121f217b4
Yu Jia Tay <yujiatay@gmail.com>
2018-10-17 01:50:00 +0800

    Update Anakin sample data

--- src/main/java/seedu/address/model/util/AnakinSampleDataUtil.java
+++ src/main/java/seedu/address/model/util/AnakinSampleDataUtil.java
@@ -12,43 +12,29 @@ import seedu.address.model.anakindeck.Name;
  * Contains utility methods for populating {@code Anakin} with sample decks.
  */
 public class AnakinSampleDataUtil {
-    private static AnakinDeck[] getSampleDecks() {
-        return new AnakinDeck[] {
-            new AnakinDeck(new Name("Asking Questions"))
-        };
-    }
-
-    private static Anakin getSampleCards(Anakin sampleAnakin) {
-        for (AnakinDeck sampleDeck : sampleAnakin.getDeckList()) {
-            sampleAnakin.getIntoDeck(sampleDeck);
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What is always coming, but never arrives?"),
-                    new AnakinAnswer("Tomorrow")));
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What can be broken, but is never held?"),
-                    new AnakinAnswer("A promise")));
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What is it that lives if it is fed, and dies if "
-                    + "you give it a drink?"), new AnakinAnswer("Fire")));
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What can one catch that is not thrown?"),
-                    new AnakinAnswer("A cold")));
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion(" What is it that if you have, you want to share "
-                    + "me, and if you share, you do not have?"), new AnakinAnswer("A secret")));
-            sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("If it takes eight men ten hours to build a wall, "
-                    + "how long would it take four men?"), new AnakinAnswer("No time, because the wall is already built"
-                    + ".")));
-        }
-        return sampleAnakin;
-    }
-
-    private static Anakin initSampleAnakin() {
-        Anakin sampleAnakin = new Anakin();
-        for (AnakinDeck sampleDeck : getSampleDecks()) {
-            sampleAnakin.addDeck(sampleDeck);
-        }
+    private static Anakin addSampleDeck(Anakin sampleAnakin) {
+        AnakinDeck sampleDeck = new AnakinDeck(new Name("Asking Questions"));
+        sampleAnakin.addDeck(sampleDeck);
+        sampleAnakin.getIntoDeck(sampleDeck);
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What is always coming, but never arrives?"),
+                new AnakinAnswer("Tomorrow")));
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What can be broken, but is never held?"),
+                new AnakinAnswer("A promise")));
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What is it that lives if it is fed, and dies if "
+                + "you give it a drink?"), new AnakinAnswer("Fire")));
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("What can one catch that is not thrown?"),
+                new AnakinAnswer("A cold")));
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion(" What is it that if you have, you want to share "
+                + "me, and if you share, you do not have?"), new AnakinAnswer("A secret")));
+        sampleAnakin.addCard(new AnakinCard(new AnakinQuestion("If it takes eight men ten hours to build a wall, "
+                + "how long would it take four men?"), new AnakinAnswer("No time, because the wall is already built"
+                + ".")));
         return sampleAnakin;
     }
 
     public static AnakinReadOnlyAnakin getSampleAnakin() {
-        Anakin sampleAnakin = initSampleAnakin();
-        sampleAnakin = getSampleCards(sampleAnakin);
+        Anakin sampleAnakin = new Anakin();
+        addSampleDeck(sampleAnakin);
         return sampleAnakin;
     }
 }
