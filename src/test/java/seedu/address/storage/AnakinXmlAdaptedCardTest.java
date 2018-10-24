9f4766d2ba56ab6ff34b889adcde3009160a505a
tomforge <fooguowei3@gmail.com>
2018-10-24 20:46:41 +0800

    add tests for invalid question and answers as they work now

--- src/test/java/seedu/address/storage/AnakinXmlAdaptedCardTest.java
+++ src/test/java/seedu/address/storage/AnakinXmlAdaptedCardTest.java
@@ -1,14 +1,16 @@
 package seedu.address.storage;
 
+import static org.junit.Assert.assertEquals;
+import static seedu.address.storage.AnakinXmlAdaptedCard.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.AnakinTypicalCards.CARD_A;
+
 import org.junit.Test;
 
+import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.model.anakindeck.AnakinAnswer;
 import seedu.address.model.anakindeck.AnakinQuestion;
 import seedu.address.testutil.Assert;
-import seedu.address.commons.exceptions.IllegalValueException;
-import static seedu.address.testutil.AnakinTypicalCards.CARD_A;
-import static org.junit.Assert.assertEquals;
-import static seedu.address.storage.AnakinXmlAdaptedCard.MISSING_FIELD_MESSAGE_FORMAT;
+
 public class AnakinXmlAdaptedCardTest {
     private static final String INVALID_QUESTION = " ";
     private static final String INVALID_ANSWER = " ";
@@ -20,26 +22,28 @@ public class AnakinXmlAdaptedCardTest {
         AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(CARD_A);
         assertEquals(CARD_A, card.toModelType());
     }
-//    TODO: There are no invalid questions yet.
-//    @Test
-//    public void toModelType_invalidQuestion_throwsIllegalValueException() {
-//        AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(INVALID_QUESTION, VALID_ANSWER);
-//        String expectedMessage = AnakinQuestion.MESSAGE_QUESTION_CONSTRAINTS;
-//        Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
-//    }
+
+    @Test
+    public void toModelType_invalidQuestion_throwsIllegalValueException() {
+        AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(INVALID_QUESTION, VALID_ANSWER);
+        String expectedMessage = AnakinQuestion.MESSAGE_QUESTION_CONSTRAINTS;
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
+    }
+
     @Test
     public void toModelType_nullQuestion_throwsIllegalValueException() {
         AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(null, VALID_ANSWER);
         String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, AnakinQuestion.class.getSimpleName());
         Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
     }
-//    TODO: There are no invalid answers yet.
-//    @Test
-//    public void toModelType_invalidAnswer_throwsIllegalValueException() {
-//        AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(VALID_QUESTION, INVALID_ANSWER);
-//        String expectedMessage = AnakinAnswer.MESSAGE_ANSWER_CONSTRAINTS;
-//        Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
-//    }
+
+    @Test
+    public void toModelType_invalidAnswer_throwsIllegalValueException() {
+        AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(VALID_QUESTION, INVALID_ANSWER);
+        String expectedMessage = AnakinAnswer.MESSAGE_ANSWER_CONSTRAINTS;
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, card::toModelType);
+    }
+
     @Test
     public void toModelType_nullAnswer_throwsIllegalValueException() {
         AnakinXmlAdaptedCard card = new AnakinXmlAdaptedCard(VALID_QUESTION, null);
