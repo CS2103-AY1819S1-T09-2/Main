0657b02cdd511213d3a295edcd94a5d0d7537879
tomforge <fooguowei3@gmail.com>
2018-10-24 20:31:35 +0800

    add xml model tests

---
+++ src/test/java/seedu/address/storage/AnakinXmlAdaptedDeckTest.java
@@ -0,0 +1,43 @@
+package seedu.address.storage;
+
+import static org.junit.Assert.assertEquals;
+import static seedu.address.storage.AnakinXmlAdaptedDeck.MISSING_FIELD_MESSAGE_FORMAT;
+import static seedu.address.testutil.AnakinTypicalDecks.DECK_A;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Test;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.model.anakindeck.Name;
+import seedu.address.testutil.Assert;
+
+public class AnakinXmlAdaptedDeckTest {
+    private static final String INVALID_NAME = " ";
+
+    private static final String VALID_NAME = DECK_A.getName().toString();
+    private static final List<AnakinXmlAdaptedCard> VALID_CARDS = DECK_A.getCards()
+            .internalList.stream()
+            .map(AnakinXmlAdaptedCard::new).collect(Collectors.toList());
+
+    @Test
+    public void toModelType_validDeckDetails_returnsDeck() throws Exception {
+        AnakinXmlAdaptedDeck deck = new AnakinXmlAdaptedDeck(DECK_A);
+        assertEquals(DECK_A, deck.toModelType());
+    }
+
+    @Test
+    public void toModelType_invalidName_throwsIllegalValueException() {
+        AnakinXmlAdaptedDeck deck = new AnakinXmlAdaptedDeck(INVALID_NAME, VALID_CARDS);
+        String expectedMessage = Name.MESSAGE_NAME_CONSTRAINTS;
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, deck::toModelType);
+    }
+
+    @Test
+    public void toModelType_nullName_throwsIllegalValueException() {
+        AnakinXmlAdaptedDeck deck = new AnakinXmlAdaptedDeck(null, VALID_CARDS);
+        String expectedMessage = String.format(MISSING_FIELD_MESSAGE_FORMAT, Name.class.getSimpleName());
+        Assert.assertThrows(IllegalValueException.class, expectedMessage, deck::toModelType);
+    }
+}
---
+++ src/test/java/seedu/address/storage/AnakinXmlSerializableAnakinTest.java
@@ -0,0 +1,49 @@
+package seedu.address.storage;
+
+import static org.junit.Assert.assertEquals;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import seedu.address.commons.exceptions.IllegalValueException;
+import seedu.address.commons.util.XmlUtil;
+import seedu.address.model.Anakin;
+import seedu.address.testutil.AnakinTypicalDecks;
+
+public class AnakinXmlSerializableAnakinTest {
+    private static final Path TEST_DATA_FOLDER = Paths.get("src", "test", "data",
+            "AnakinXmlSerializableAnakinTest");
+    private static final Path TYPICAL_DECKS_FILE = TEST_DATA_FOLDER.resolve("typicalDecksAnakin.xml");
+    private static final Path INVALID_DECK_FILE = TEST_DATA_FOLDER.resolve("invalidDeckAnakin.xml");
+    private static final Path DUPLICATE_DECK_FILE = TEST_DATA_FOLDER.resolve("duplicateDeckAnakin.xml");
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void toModelType_typicalDecksFile_success() throws Exception {
+        AnakinXmlSerializableAnakin dataFromFile = XmlUtil.getDataFromFile(TYPICAL_DECKS_FILE,
+                AnakinXmlSerializableAnakin.class);
+        Anakin anakinFromFile = dataFromFile.toModelType();
+        Anakin typicalDecksAnakin = AnakinTypicalDecks.getTypicalAnakin();
+        assertEquals(anakinFromFile, typicalDecksAnakin);
+    }
+    @Test
+    public void toModelType_invalidDecksFile_throwsIllegalValueException() throws Exception {
+        AnakinXmlSerializableAnakin dataFromFile = XmlUtil.getDataFromFile(INVALID_DECK_FILE,
+                AnakinXmlSerializableAnakin.class);
+        thrown.expect(IllegalValueException.class);
+        dataFromFile.toModelType();
+    }
+    @Test
+    public void toModelType_duplicateDecksFile_throwsIllegalValueException() throws Exception {
+        AnakinXmlSerializableAnakin dataFromFile = XmlUtil.getDataFromFile(DUPLICATE_DECK_FILE,
+                AnakinXmlSerializableAnakin.class);
+        thrown.expect(IllegalValueException.class);
+        thrown.expectMessage(AnakinXmlSerializableAnakin.MESSAGE_DUPLICATE_DECK);
+        dataFromFile.toModelType();
+    }
+}
