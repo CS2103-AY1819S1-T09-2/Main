a10076a2863d0e3af8f53ba06cccf626aa1c2c42
Yu Jia Tay <yujiatay@gmail.com>
2018-10-26 17:50:44 +0800

    Refactor other unit tests to use new clear command

--- src/test/java/seedu/address/logic/anakincommands/EditCardCommandTest.java
+++ src/test/java/seedu/address/logic/anakincommands/EditCardCommandTest.java
@@ -19,7 +19,6 @@ import seedu.address.commons.core.AddressbookMessages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.anakincommands.EditCardCommand.EditCardDescriptor;
-import seedu.address.logic.commands.ClearCommand;
 import seedu.address.model.Anakin;
 import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
