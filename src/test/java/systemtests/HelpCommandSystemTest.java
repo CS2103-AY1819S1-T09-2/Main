2892e16ab0220da51400aed390b46ae4e7180c12
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2018-10-28 21:44:59 +0800

    Add initial systemtests for help command

--- src/test/java/systemtests/HelpCommandSystemTest.java
+++ src/test/java/systemtests/HelpCommandSystemTest.java
@@ -1,18 +1,40 @@
 package systemtests;
 
-//import static seedu.address.ui.testutil.GuiTestAssert.assertListMatching;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+//import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+
+//import static seedu.address.testutil.TypicalDecks.DECK_A;
+//
+//
+//import static seedu.address.ui.testutil.GuiTestAssert.assertDeckListMatching;
+
+import org.junit.Test;
+
+import guitests.GuiRobot;
+import guitests.guihandles.HelpWindowHandle;
+//import seedu.address.logic.commands.DeleteDeckCommand;
+//import seedu.address.logic.commands.NewDeckCommand;
+import seedu.address.logic.commands.HelpCommand;
+//import seedu.address.testutil.TypicalDecks;
+//import seedu.address.ui.BrowserPanel;
+//import seedu.address.ui.StatusBarFooter;
+
+
+//import static seedu.address.ui.testutil.GuiTestAssert.assertDeckListMatching;
 
 /**
  * A system test class for the help window, which contains interaction with other UI components.
  */
 public class HelpCommandSystemTest extends AnakinSystemTest {
-//    private static final String ERROR_MESSAGE = "ATTENTION!!!! : On some computers, this test may fail when run on "
-//            + "non-headless mode as FxRobot#clickOn(Node, MouseButton...) clicks on the wrong location. We suspect "
-//            + "that this is a bug with TestFX library that we are using. If this test fails, you have to run your "
-//            + "tests on headless mode. See UsingGradle.adoc on how to do so.";
-//
-//    private final GuiRobot guiRobot = new GuiRobot();
-//
+    private static final String ERROR_MESSAGE = "ATTENTION!!!! : On some computers, this test may fail when run on "
+            + "non-headless mode as FxRobot#clickOn(Node, MouseButton...) clicks on the wrong location. We suspect "
+            + "that this is a bug with TestFX library that we are using. If this test fails, you have to run your "
+            + "tests on headless mode. See UsingGradle.adoc on how to do so.";
+
+    private final GuiRobot guiRobot = new GuiRobot();
+
 //    @Test
 //    public void openHelpWindow() {
 //        //use accelerator
@@ -24,7 +46,7 @@ public class HelpCommandSystemTest extends AnakinSystemTest {
 //        getMainMenu().openHelpWindowUsingAccelerator();
 //        assertHelpWindowOpen();
 //
-//        getPersonListPanel().click();
+//        getDeckListPanel().click();
 //        getMainMenu().openHelpWindowUsingAccelerator();
 //        assertHelpWindowOpen();
 //
@@ -45,48 +67,48 @@ public class HelpCommandSystemTest extends AnakinSystemTest {
 //        getMainWindowHandle().focus();
 //
 //        // assert that while the help window is open the UI updates correctly for a command execution
-//        executeCommand(SelectCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+//        executeCommand(NewDeckCommand.COMMAND_WORD + " " + TypicalDecks.DECK_A);
 //        assertEquals("", getCommandBox().getInput());
 //        assertCommandBoxShowsDefaultStyle();
 //        assertNotEquals(HelpCommand.SHOWING_HELP_MESSAGE, getResultDisplay().getText());
 //        assertNotEquals(BrowserPanel.DEFAULT_PAGE, getBrowserPanel().getLoadedUrl());
-//        assertListMatching(getPersonListPanel(), getModel().getFilteredPersonList());
+//        assertDeckListMatching(getDeckListPanel(), getModel().getFilteredDeckList());
 //
 //        // assert that the status bar too is updated correctly while the help window is open
 //        // note: the select command tested above does not update the status bar
-//        executeCommand(DeleteCommand.COMMAND_WORD + " " + INDEX_FIRST_PERSON.getOneBased());
+//        executeCommand(DeleteDeckCommand.COMMAND_WORD + " " + DECK_A);
 //        assertNotEquals(StatusBarFooter.SYNC_STATUS_INITIAL, getStatusBarFooter().getSyncStatus());
 //    }
-//
-//    @Test
-//    public void help_multipleCommands_onlyOneHelpWindowOpen() {
-//        getMainMenu().openHelpWindowUsingMenu();
-//
-//        getMainWindowHandle().focus();
-//        getMainMenu().openHelpWindowUsingAccelerator();
-//
-//        getMainWindowHandle().focus();
-//        executeCommand(HelpCommand.COMMAND_WORD);
-//
-//        assertEquals(1, guiRobot.getNumberOfWindowsShown(HelpWindowHandle.HELP_WINDOW_TITLE));
-//    }
-//
-//    /**
-//     * Asserts that the help window is open, and closes it after checking.
-//     */
-//    private void assertHelpWindowOpen() {
-//        assertTrue(ERROR_MESSAGE, HelpWindowHandle.isWindowPresent());
-//        guiRobot.pauseForHuman();
-//
-//        new HelpWindowHandle(guiRobot.getStage(HelpWindowHandle.HELP_WINDOW_TITLE)).close();
-//        getMainWindowHandle().focus();
-//    }
-//
-//    /**
-//     * Asserts that the help window isn't open.
-//     */
-//    private void assertHelpWindowNotOpen() {
-//        assertFalse(ERROR_MESSAGE, HelpWindowHandle.isWindowPresent());
-//    }
+
+    @Test
+    public void help_multipleCommands_onlyOneHelpWindowOpen() {
+        getMainMenu().openHelpWindowUsingMenu();
+
+        getMainWindowHandle().focus();
+        getMainMenu().openHelpWindowUsingAccelerator();
+
+        getMainWindowHandle().focus();
+        executeCommand(HelpCommand.COMMAND_WORD);
+
+        assertEquals(1, guiRobot.getNumberOfWindowsShown(HelpWindowHandle.HELP_WINDOW_TITLE));
+    }
+
+    /**
+     * Asserts that the help window is open, and closes it after checking.
+     */
+    private void assertHelpWindowOpen() {
+        assertTrue(ERROR_MESSAGE, HelpWindowHandle.isWindowPresent());
+        guiRobot.pauseForHuman();
+
+        new HelpWindowHandle(guiRobot.getStage(HelpWindowHandle.HELP_WINDOW_TITLE)).close();
+        getMainWindowHandle().focus();
+    }
+
+    /**
+     * Asserts that the help window isn't open.
+     */
+    private void assertHelpWindowNotOpen() {
+        assertFalse(ERROR_MESSAGE, HelpWindowHandle.isWindowPresent());
+    }
 
 }
