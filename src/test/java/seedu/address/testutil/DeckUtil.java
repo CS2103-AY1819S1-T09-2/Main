5a6e3a07fa7aac570f40deaa4a6d90cca717eac7
Lee Yi Jie Joel <joel@Lees-MacBook-Pro-2.local>
2018-11-04 22:26:35 +0800

    Add Deckutil

---
+++ src/test/java/seedu/address/testutil/DeckUtil.java
@@ -0,0 +1,33 @@
+package seedu.address.testutil;
+
+
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+
+
+import java.util.Set;
+
+import seedu.address.logic.commands.NewDeckCommand;
+import seedu.address.model.deck.Deck;
+
+/**
+ * A utility class for Deck.
+ */
+public class DeckUtil {
+
+    /**
+     * Returns a new deck command string for adding the {@code person}.
+     */
+    public static String getNewDeckCommand(Deck deck) {
+        return NewDeckCommand.COMMAND_WORD + " " + getDeckDetails(deck);
+    }
+
+    /**
+     * Returns the part of command string for the given {@code person}'s details.
+     */
+    public static String getDeckDetails(Deck deck) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(PREFIX_NAME + deck.getName().fullName + " ");
+        return sb.toString();
+    }
+
+}
